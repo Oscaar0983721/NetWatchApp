766885652043efeb7f66f6768eba82ae41e744a8
Andrés Araya Saborío <142752659+XAnderXZ@users.noreply.github.com>
2025-03-31 21:53:55 -0600

    Create ViewingHistory.cs

---
+++ NetWatchApp/Classes/Models/ViewingHistory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NetWatchApp.Classes.Models
+{
+    public class ViewingHistory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime ViewDate { get; set; }
+
+        public int WatchedMinutes { get; set; }
+
+        public bool Completed { get; set; }
+
+        // Foreign keys
+        public int UserId { get; set; }
+        public int ContentId { get; set; }
+        public int? EpisodeId { get; set; }
+
+        // Navigation properties
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+
+        [ForeignKey("ContentId")]
+        public virtual Content Content { get; set; }
+
+        [ForeignKey("EpisodeId")]
+        public virtual Episode Episode { get; set; }
+    }
+}
