5d0e9cdb46e666687fd57985962923396183137e
Andrés Araya Saborío <142752659+XAnderXZ@users.noreply.github.com>
2025-03-31 21:56:23 -0600

    Create IViewingHistoryRepository.cs

---
+++ NetWatchApp/Interfaces/IViewingHistoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using NetWatchApp.Classes.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NetWatchApp.Interfaces
+{
+    public interface IViewingHistoryRepository
+    {
+        Task<ViewingHistory> GetByIdAsync(int id);
+        Task<IEnumerable<ViewingHistory>> GetByUserIdAsync(int userId);
+        Task<IEnumerable<ViewingHistory>> GetByContentIdAsync(int contentId);
+        Task<bool> AddAsync(ViewingHistory viewingHistory);
+        Task<bool> UpdateAsync(ViewingHistory viewingHistory);
+        Task<bool> DeleteAsync(int id);
+        Task<int> GetTotalWatchTimeForUserAsync(int userId);
+    }
+}
