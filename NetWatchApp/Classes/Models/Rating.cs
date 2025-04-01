d9d433a3932979ae88b88105447f0ba5a158e078
Andrés Araya Saborío <142752659+XAnderXZ@users.noreply.github.com>
2025-03-31 21:54:36 -0600

    Create Rating.cs

---
+++ NetWatchApp/Classes/Models/Rating.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NetWatchApp.Classes.Models
+{
+    public class Rating
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int Score { get; set; }
+
+        [StringLength(500)]
+        public string Comment { get; set; }
+
+        public DateTime RatingDate { get; set; }
+
+        // Foreign keys
+        public int UserId { get; set; }
+        public int ContentId { get; set; }
+
+        // Navigation properties
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+
+        [ForeignKey("ContentId")]
+        public virtual Content Content { get; set; }
+    }
+}
