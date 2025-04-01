266fb41bb09388a1058ad11214c1ff4cdc75baed
Andrés Araya Saborío <142752659+XAnderXZ@users.noreply.github.com>
2025-03-31 21:51:25 -0600

    Agregar archivos de proyecto.

---
+++ NetWatchApp/Classes/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace NetWatchApp.Classes.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string IdentificationNumber { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        public bool IsAdmin { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+
+        // Navigation properties
+        public virtual ICollection<ViewingHistory> ViewingHistories { get; set; }
+        public virtual ICollection<Rating> Ratings { get; set; }
+    }
+}
