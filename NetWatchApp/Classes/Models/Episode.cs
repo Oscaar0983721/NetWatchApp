ea1ac44d20827abf741fb6b6c020244d8214e314
Andrés Araya Saborío <142752659+XAnderXZ@users.noreply.github.com>
2025-03-31 21:53:17 -0600

    Create Episode.cs

---
+++ NetWatchApp/Classes/Models/Episode.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NetWatchApp.Classes.Models
+{
+    public class Episode
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        public int SeasonNumber { get; set; }
+
+        public int EpisodeNumber { get; set; }
+
+        public int DurationMinutes { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        // Foreign key
+        public int ContentId { get; set; }
+
+        // Navigation property
+        [ForeignKey("ContentId")]
+        public virtual Content Content { get; set; }
+    }
+}
