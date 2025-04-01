14cb93111b22099141901b520c0d7786a4715f32
Andrés Araya Saborío <142752659+XAnderXZ@users.noreply.github.com>
2025-03-31 21:57:07 -0600

    Create IRatingRepository.cs

---
+++ NetWatchApp/Interfaces/IRatingRepository.cs
@@ -0,0 +1,17 @@
+﻿using NetWatchApp.Classes.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NetWatchApp.Interfaces
+{
+    public interface IRatingRepository
+    {
+        Task<Rating> GetByIdAsync(int id);
+        Task<IEnumerable<Rating>> GetByUserIdAsync(int userId);
+        Task<IEnumerable<Rating>> GetByContentIdAsync(int contentId);
+        Task<double> GetAverageRatingForContentAsync(int contentId);
+        Task<bool> AddAsync(Rating rating);
+        Task<bool> UpdateAsync(Rating rating);
+        Task<bool> DeleteAsync(int id);
+    }
+}
