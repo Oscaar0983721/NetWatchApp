38141f70a09a944e5d452e38798d6fccaaa08265
Andrés Araya Saborío <142752659+XAnderXZ@users.noreply.github.com>
2025-03-31 21:56:26 -0600

    Create IContentRepository.cs

---
+++ NetWatchApp/Interfaces/IContentRepository.cs
@@ -0,0 +1,18 @@
+﻿using NetWatchApp.Classes.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NetWatchApp.Interfaces
+{
+    public interface IContentRepository
+    {
+        Task<Content> GetByIdAsync(int id);
+        Task<IEnumerable<Content>> GetAllAsync();
+        Task<IEnumerable<Content>> GetByTypeAsync(ContentType type);
+        Task<IEnumerable<Content>> GetByGenreAsync(string genre);
+        Task<IEnumerable<Content>> SearchAsync(string searchTerm);
+        Task<bool> AddAsync(Content content);
+        Task<bool> UpdateAsync(Content content);
+        Task<bool> DeleteAsync(int id);
+    }
+}
