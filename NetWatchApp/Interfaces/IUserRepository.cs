5e1ba1bec14868f9d0ea16d5c41de7113fe3d17e
Andrés Araya Saborío <142752659+XAnderXZ@users.noreply.github.com>
2025-03-31 21:55:10 -0600

    Create IUserRepository.cs

---
+++ NetWatchApp/Interfaces/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using NetWatchApp.Classes.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NetWatchApp.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> GetByIdAsync(int id);
+        Task<User> GetByIdentificationNumberAsync(string identificationNumber);
+        Task<IEnumerable<User>> GetAllAsync();
+        Task<bool> AddAsync(User user);
+        Task<bool> UpdateAsync(User user);
+        Task<bool> DeleteAsync(int id);
+        Task<bool> ValidateCredentialsAsync(string identificationNumber, string password);
+    }
+}
