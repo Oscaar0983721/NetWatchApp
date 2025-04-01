5c7af8e3acf4964fd88461274a08a62730cb4616
Andrés Araya Saborío <142752659+XAnderXZ@users.noreply.github.com>
2025-03-31 22:02:50 -0600

    Create LoginForm.cs

---
+++ NetWatchApp/Forms/LoginForm.cs
@@ -0,0 +1,176 @@
+﻿using NetWatchApp.Classes.Models;
+using NetWatchApp.Classes.Repositories;
+using NetWatchApp.Data.EntityFramework;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace NetWatchApp.Forms
+{
+    public partial class LoginForm : Form
+    {
+        private readonly UserRepository _userRepository;
+
+        public LoginForm()
+        {
+            InitializeComponent();
+            _userRepository = new UserRepository(new NetWatchDbContext());
+        }
+
+        private void InitializeComponent()
+        {
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblIdentificationNumber = new System.Windows.Forms.Label();
+            this.txtIdentificationNumber = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.pnlHeader.SuspendLayout();
+            this.SuspendLayout();
+
+            // pnlHeader
+            this.pnlHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.pnlHeader.Controls.Add(this.lblTitle);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(400, 80);
+            this.pnlHeader.TabIndex = 0;
+
+            // lblTitle
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(20, 25);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(360, 32);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "NetWatch - Login";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+            // lblIdentificationNumber
+            this.lblIdentificationNumber.AutoSize = true;
+            this.lblIdentificationNumber.Location = new System.Drawing.Point(50, 120);
+            this.lblIdentificationNumber.Name = "lblIdentificationNumber";
+            this.lblIdentificationNumber.Size = new System.Drawing.Size(127, 15);
+            this.lblIdentificationNumber.TabIndex = 1;
+            this.lblIdentificationNumber.Text = "Identification Number:";
+
+            // txtIdentificationNumber
+            this.txtIdentificationNumber.Location = new System.Drawing.Point(50, 140);
+            this.txtIdentificationNumber.Name = "txtIdentificationNumber";
+            this.txtIdentificationNumber.Size = new System.Drawing.Size(300, 23);
+            this.txtIdentificationNumber.TabIndex = 2;
+
+            // lblPassword
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(50, 180);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(60, 15);
+            this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "Password:";
+
+            // txtPassword
+            this.txtPassword.Location = new System.Drawing.Point(50, 200);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(300, 23);
+            this.txtPassword.TabIndex = 4;
+
+            // btnLogin
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(50, 250);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(300, 40);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+
+            // btnRegister
+            this.btnRegister.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegister.Location = new System.Drawing.Point(50, 300);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(300, 40);
+            this.btnRegister.TabIndex = 6;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+
+            // LoginForm
+            this.ClientSize = new System.Drawing.Size(400, 380);
+            this.Controls.Add(this.pnlHeader);
+            this.Controls.Add(this.lblIdentificationNumber);
+            this.Controls.Add(this.txtIdentificationNumber);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnRegister);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NetWatch - Login";
+            this.pnlHeader.ResumeLayout(false);
+            this.pnlHeader.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Panel pnlHeader;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblIdentificationNumber;
+        private System.Windows.Forms.TextBox txtIdentificationNumber;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnRegister;
+
+        private async void btnLogin_Click(object sender, EventArgs e)
+        {
+            string identificationNumber = txtIdentificationNumber.Text.Trim();
+            string password = txtPassword.Text;
+
+            if (string.IsNullOrEmpty(identificationNumber) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Please enter both identification number and password.", "Login Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            bool isValid = await _userRepository.ValidateCredentialsAsync(identificationNumber, password);
+
+            if (isValid)
+            {
+                var user = await _userRepository.GetByIdentificationNumberAsync(identificationNumber);
+
+                // Hide login form
+                this.Hide();
+
+                // Show main form
+                MainForm mainForm = new MainForm(user);
+                mainForm.FormClosed += (s, args) => this.Close();
+                mainForm.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid identification number or password.", "Login Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            // Hide login form
+            this.Hide();
+
+            // Show register form
+            RegisterForm registerForm = new RegisterForm();
+            registerForm.FormClosed += (s, args) => this.Show();
+            registerForm.Show();
+        }
+    }
+}
