83b7025670ec8d8e1cebf4762eb262aa72925d87
Andrés Araya Saborío <142752659+Oscaar0983721@users.noreply.github.com>
2025-04-03 18:14:27 -0600

    Update RegisterForm.cs

--- NetWatchApp/Forms/RegisterForm.cs
+++ NetWatchApp/Forms/RegisterForm.cs
@@ -1,6 +1,5 @@
 ﻿using NetWatchApp.Classes.Models;
 using NetWatchApp.Classes.Repositories;
-using NetWatchApp.Data.EntityFramework;
 using System;
 using System.Windows.Forms;
 
@@ -21,14 +20,23 @@ namespace NetWatchApp.Forms
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Label lblConfirmPassword;
         private System.Windows.Forms.TextBox txtConfirmPassword;
+        private System.Windows.Forms.CheckBox chkIsAdmin;
         private System.Windows.Forms.Button btnRegister;
         private System.Windows.Forms.Button btnCancel;
-        private readonly UserRepository _userRepository;
 
-        public RegisterForm()
+        private readonly UserRepository _userRepository;
+        private readonly bool _isAdminCreating;
+
+        public string RegisteredEmail { get; private set; }
+
+        public RegisterForm(bool isAdminCreating = false)
         {
             InitializeComponent();
-            _userRepository = new UserRepository(new NetWatchDbContext());
+            _userRepository = new UserRepository(new Data.EntityFramework.NetWatchDbContext());
+            _isAdminCreating = isAdminCreating;
+
+            // Only show admin checkbox if an admin is creating the user
+            chkIsAdmin.Visible = _isAdminCreating;
 
             // Set up event handlers
             btnRegister.Click += BtnRegister_Click;
@@ -50,6 +58,7 @@ namespace NetWatchApp.Forms
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.lblConfirmPassword = new System.Windows.Forms.Label();
             this.txtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.chkIsAdmin = new System.Windows.Forms.CheckBox();
             this.btnRegister = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
 
@@ -59,98 +68,107 @@ namespace NetWatchApp.Forms
             this.lblTitle.Location = new System.Drawing.Point(100, 20);
             this.lblTitle.Name = "lblTitle";
             this.lblTitle.Size = new System.Drawing.Size(200, 37);
-            this.lblTitle.Text = "Register Account";
+            this.lblTitle.Text = "Register User";
 
             // lblIdentificationNumber
             this.lblIdentificationNumber.AutoSize = true;
-            this.lblIdentificationNumber.Location = new System.Drawing.Point(30, 80);
+            this.lblIdentificationNumber.Location = new System.Drawing.Point(20, 80);
             this.lblIdentificationNumber.Name = "lblIdentificationNumber";
-            this.lblIdentificationNumber.Size = new System.Drawing.Size(150, 20);
-            this.lblIdentificationNumber.Text = "Identification Number:";
+            this.lblIdentificationNumber.Size = new System.Drawing.Size(100, 20);
+            this.lblIdentificationNumber.Text = "ID Number:";
 
             // txtIdentificationNumber
-            this.txtIdentificationNumber.Location = new System.Drawing.Point(180, 80);
+            this.txtIdentificationNumber.Location = new System.Drawing.Point(150, 80);
             this.txtIdentificationNumber.Name = "txtIdentificationNumber";
             this.txtIdentificationNumber.Size = new System.Drawing.Size(200, 27);
 
             // lblFirstName
             this.lblFirstName.AutoSize = true;
-            this.lblFirstName.Location = new System.Drawing.Point(30, 120);
+            this.lblFirstName.Location = new System.Drawing.Point(20, 120);
             this.lblFirstName.Name = "lblFirstName";
             this.lblFirstName.Size = new System.Drawing.Size(80, 20);
             this.lblFirstName.Text = "First Name:";
 
             // txtFirstName
-            this.txtFirstName.Location = new System.Drawing.Point(180, 120);
+            this.txtFirstName.Location = new System.Drawing.Point(150, 120);
             this.txtFirstName.Name = "txtFirstName";
             this.txtFirstName.Size = new System.Drawing.Size(200, 27);
 
             // lblLastName
             this.lblLastName.AutoSize = true;
-            this.lblLastName.Location = new System.Drawing.Point(30, 160);
+            this.lblLastName.Location = new System.Drawing.Point(20, 160);
             this.lblLastName.Name = "lblLastName";
-            this.lblLastName.Size = new System.Drawing.Size(79, 20);
+            this.lblLastName.Size = new System.Drawing.Size(80, 20);
             this.lblLastName.Text = "Last Name:";
 
             // txtLastName
-            this.txtLastName.Location = new System.Drawing.Point(180, 160);
+            this.txtLastName.Location = new System.Drawing.Point(150, 160);
             this.txtLastName.Name = "txtLastName";
             this.txtLastName.Size = new System.Drawing.Size(200, 27);
 
             // lblEmail
             this.lblEmail.AutoSize = true;
-            this.lblEmail.Location = new System.Drawing.Point(30, 200);
+            this.lblEmail.Location = new System.Drawing.Point(20, 200);
             this.lblEmail.Name = "lblEmail";
             this.lblEmail.Size = new System.Drawing.Size(46, 20);
             this.lblEmail.Text = "Email:";
 
             // txtEmail
-            this.txtEmail.Location = new System.Drawing.Point(180, 200);
+            this.txtEmail.Location = new System.Drawing.Point(150, 200);
             this.txtEmail.Name = "txtEmail";
             this.txtEmail.Size = new System.Drawing.Size(200, 27);
 
             // lblPassword
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(30, 240);
+            this.lblPassword.Location = new System.Drawing.Point(20, 240);
             this.lblPassword.Name = "lblPassword";
             this.lblPassword.Size = new System.Drawing.Size(73, 20);
             this.lblPassword.Text = "Password:";
 
             // txtPassword
-            this.txtPassword.Location = new System.Drawing.Point(180, 240);
+            this.txtPassword.Location = new System.Drawing.Point(150, 240);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
             this.txtPassword.Size = new System.Drawing.Size(200, 27);
 
             // lblConfirmPassword
             this.lblConfirmPassword.AutoSize = true;
-            this.lblConfirmPassword.Location = new System.Drawing.Point(30, 280);
+            this.lblConfirmPassword.Location = new System.Drawing.Point(20, 280);
             this.lblConfirmPassword.Name = "lblConfirmPassword";
-            this.lblConfirmPassword.Size = new System.Drawing.Size(132, 20);
+            this.lblConfirmPassword.Size = new System.Drawing.Size(130, 20);
             this.lblConfirmPassword.Text = "Confirm Password:";
 
             // txtConfirmPassword
-            this.txtConfirmPassword.Location = new System.Drawing.Point(180, 280);
+            this.txtConfirmPassword.Location = new System.Drawing.Point(150, 280);
             this.txtConfirmPassword.Name = "txtConfirmPassword";
             this.txtConfirmPassword.PasswordChar = '*';
             this.txtConfirmPassword.Size = new System.Drawing.Size(200, 27);
 
+            // chkIsAdmin
+            this.chkIsAdmin.AutoSize = true;
+            this.chkIsAdmin.Location = new System.Drawing.Point(150, 320);
+            this.chkIsAdmin.Name = "chkIsAdmin";
+            this.chkIsAdmin.Size = new System.Drawing.Size(150, 24);
+            this.chkIsAdmin.Text = "Administrator User";
+            this.chkIsAdmin.UseVisualStyleBackColor = true;
+            this.chkIsAdmin.Visible = false; // Only visible when admin is creating
+
             // btnRegister
-            this.btnRegister.Location = new System.Drawing.Point(180, 330);
+            this.btnRegister.Location = new System.Drawing.Point(100, 360);
             this.btnRegister.Name = "btnRegister";
             this.btnRegister.Size = new System.Drawing.Size(100, 35);
             this.btnRegister.Text = "Register";
             this.btnRegister.UseVisualStyleBackColor = true;
 
             // btnCancel
-            this.btnCancel.Location = new System.Drawing.Point(290, 330);
+            this.btnCancel.Location = new System.Drawing.Point(220, 360);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(100, 35);
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
 
             // RegisterForm
-            this.ClientSize = new System.Drawing.Size(420, 390);
+            this.ClientSize = new System.Drawing.Size(400, 420);
             this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.lblIdentificationNumber);
             this.Controls.Add(this.txtIdentificationNumber);
@@ -164,6 +182,7 @@ namespace NetWatchApp.Forms
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.lblConfirmPassword);
             this.Controls.Add(this.txtConfirmPassword);
+            this.Controls.Add(this.chkIsAdmin);
             this.Controls.Add(this.btnRegister);
             this.Controls.Add(this.btnCancel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -171,7 +190,7 @@ namespace NetWatchApp.Forms
             this.MinimizeBox = false;
             this.Name = "RegisterForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Register Account";
+            this.Text = "NetWatch App - Register";
         }
 
         private void BtnRegister_Click(object sender, EventArgs e)
@@ -187,20 +206,21 @@ namespace NetWatchApp.Forms
                         LastName = txtLastName.Text.Trim(),
                         Email = txtEmail.Text.Trim(),
                         Password = txtPassword.Text,
-                        IsAdmin = false,
+                        IsAdmin = _isAdminCreating && chkIsAdmin.Checked,
                         RegistrationDate = DateTime.Now
                     };
 
                     _userRepository.Add(user);
 
-                    MessageBox.Show("Registration successful! You can now login with your email and password.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    RegisteredEmail = user.Email;
+                    MessageBox.Show("User registered successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     DialogResult = DialogResult.OK;
                     Close();
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Error during registration: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show($"Error registering user: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
@@ -208,29 +228,29 @@ namespace NetWatchApp.Forms
         {
             if (string.IsNullOrWhiteSpace(txtIdentificationNumber.Text))
             {
-                MessageBox.Show("Please enter your identification number.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show("Please enter an identification number.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return false;
             }
 
             if (string.IsNullOrWhiteSpace(txtFirstName.Text))
             {
-                MessageBox.Show("Please enter your first name.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show("Please enter a first name.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return false;
             }
 
             if (string.IsNullOrWhiteSpace(txtLastName.Text))
             {
-                MessageBox.Show("Please enter your last name.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show("Please enter a last name.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return false;
             }
 
             if (string.IsNullOrWhiteSpace(txtEmail.Text))
             {
-                MessageBox.Show("Please enter your email address.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show("Please enter an email address.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return false;
             }
 
-            if (!txtEmail.Text.Contains("@") || !txtEmail.Text.Contains("."))
+            if (!IsValidEmail(txtEmail.Text.Trim()))
             {
                 MessageBox.Show("Please enter a valid email address.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return false;
@@ -255,6 +275,19 @@ namespace NetWatchApp.Forms
             }
 
             return true;
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         private void BtnCancel_Click(object sender, EventArgs e)
