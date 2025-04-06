56d6cc410bb4bf9de10e819b02d41cb0696048f8
Andrés Araya Saborío <saborioanaraya1106@gmail.com>
2025-04-05 19:47:26 -0600

    Update ViewingHistoryRepository.cs

--- NetWatchApp/Classes/Repositories/ViewingHistoryRepository.cs
+++ NetWatchApp/Classes/Repositories/ViewingHistoryRepository.cs
@@ -1,114 +1,63 @@
-﻿using Microsoft.EntityFrameworkCore;
-using NetWatchApp.Classes.Models;
-using NetWatchApp.Data.EntityFramework;
-using NetWatchApp.Interfaces;
+﻿using NetWatchApp.Classes.Models;
 using NetWatchApp.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace NetWatchApp.Classes.Repositories
 {
-    public class ViewingHistoryRepository : IViewingHistoryRepository
+    public class ViewingHistoryRepository
     {
-        private readonly NetWatchDbContext _context;
         private readonly JsonDataService _jsonDataService;
 
-        public ViewingHistoryRepository(NetWatchDbContext context)
+        public ViewingHistoryRepository()
         {
-            _context = context;
             _jsonDataService = new JsonDataService();
         }
 
         public List<ViewingHistory> GetAll()
         {
-            return _context.ViewingHistories
-                .Include(vh => vh.User)
-                .Include(vh => vh.Content)
-                .ToList();
+            return _jsonDataService.GetAllViewingHistories();
         }
 
         public ViewingHistory GetById(int id)
         {
-            return _context.ViewingHistories
-                .Include(vh => vh.User)
-                .Include(vh => vh.Content)
-                .FirstOrDefault(vh => vh.Id == id);
+            return _jsonDataService.GetViewingHistoryById(id);
         }
 
         public List<ViewingHistory> GetByUser(int userId)
         {
-            return _context.ViewingHistories
-                .Include(vh => vh.Content)
-                .Where(vh => vh.UserId == userId)
-                .ToList();
+            return _jsonDataService.GetViewingHistoriesByUser(userId);
         }
 
         public List<ViewingHistory> GetByContent(int contentId)
         {
-            return _context.ViewingHistories
-                .Include(vh => vh.User)
-                .Where(vh => vh.ContentId == contentId)
-                .ToList();
+            return _jsonDataService.GetViewingHistoriesByContent(contentId);
         }
 
         public ViewingHistory GetByUserAndContent(int userId, int contentId)
         {
-            return _context.ViewingHistories
-                .FirstOrDefault(vh => vh.UserId == userId && vh.ContentId == contentId);
+            return _jsonDataService.GetViewingHistoryByUserAndContent(userId, contentId);
         }
 
         public void Add(ViewingHistory viewingHistory)
         {
-            _context.ViewingHistories.Add(viewingHistory);
-            _context.SaveChanges();
-
-            // Save to JSON
-            Task.Run(async () => await _jsonDataService.SaveViewingHistoryToJsonAsync(viewingHistory))
-                .ConfigureAwait(false);
+            _jsonDataService.AddViewingHistory(viewingHistory);
         }
 
         public void Update(ViewingHistory viewingHistory)
         {
-            var existingHistory = _context.ViewingHistories.Find(viewingHistory.Id);
-            if (existingHistory == null)
-            {
-                throw new Exception($"Viewing history with ID {viewingHistory.Id} not found.");
-            }
-
-            existingHistory.WatchDate = viewingHistory.WatchDate;
-            existingHistory.WatchedEpisodes = viewingHistory.WatchedEpisodes;
-
-            _context.SaveChanges();
-
-            // Save to JSON after update
-            Task.Run(async () => await _jsonDataService.SaveViewingHistoryToJsonAsync(existingHistory))
-                .ConfigureAwait(false);
+            _jsonDataService.UpdateViewingHistory(viewingHistory);
         }
 
         public void Delete(int id)
         {
-            var viewingHistory = _context.ViewingHistories.Find(id);
-            if (viewingHistory != null)
-            {
-                _context.ViewingHistories.Remove(viewingHistory);
-                _context.SaveChanges();
-
-                // Note: We don't delete the JSON file to maintain a history
-            }
+            _jsonDataService.DeleteViewingHistory(id);
         }
 
         public List<Content> GetRecentlyWatchedContent(int userId, int count = 5)
         {
-            return _context.ViewingHistories
-                .Include(vh => vh.Content)
-                .ThenInclude(c => c.Episodes)
-                .Where(vh => vh.UserId == userId)
-                .OrderByDescending(vh => vh.WatchDate)
-                .Take(count)
-                .Select(vh => vh.Content)
-                .ToList();
+            return _jsonDataService.GetRecentlyWatchedContent(userId, count);
         }
     }
 }
